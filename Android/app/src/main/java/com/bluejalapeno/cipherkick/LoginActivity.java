62f9cdaf1625def85a261ade3ac3131c862930bc
Jason David Evans <jasondevans@users.noreply.github.com>
2016-09-26 14:09:11 -0400

    Initial Android commit.

---
+++ Android/app/src/main/java/com/bluejalapeno/cipherkick/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.bluejalapeno.cipherkick;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        // Call superclass onCreate
+        super.onCreate(savedInstanceState);
+
+        // Set our content view
+        setContentView(R.layout.login);
+
+        // Add a button click handler
+        final Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Transition to the list activity
+                // new Thread(() -> startActivity(new Intent(getApplicationContext(), ListActivity.class))).start();
+                startActivity(new Intent(getApplicationContext(), ListActivity.class));
+            }
+        });
+    }
+}
